e1e7106642570c8d33f6ef74ab4c54ef9d6f7d90
Herve Saint-Amand <git@saintamh.org>
2017-07-27 22:49:23 +0100

    These fields don't belong here
    They're already in Fetcher

--- alcazar/scraper.py
+++ alcazar/scraper.py
@@ -23,14 +23,6 @@ class Scraper(Cleaner, Fetcher):
 
     refresh_interval = timedelta(days=1)
 
-    http_cache_life = timedelta(days=30)
-    http_timeout = 30
-    http_user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-
-    html_encoding = 'UTF-8'
-    html_encoding_errors = 'strict'
-    html_do_decode_entities = False
-
     def __init__(self):
         if self.id is None:
             self.id = self.__class__.__name__
