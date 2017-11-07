0ff8a4efa37bf3ef2b6fdd31d550edf4c620ea08
Herve Saint-Amand <git@saintamh.org>
2017-11-07 20:31:03 +0000

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+from __future__ import unicode_literals
+
+from distutils.core import setup
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+setup(
+    name='Alcazar',
+    version='0.1',
+    description='Alcazar web scraping library',
+    author='Herv\u00e9 Saint-Amand',
+    author_email='alcazar@saintamh.org',
+    url='https://saintamh.org/code/alcazar/',
+    packages=['alcazar'],
+)
+
+#----------------------------------------------------------------------------------------------------------------------------------
