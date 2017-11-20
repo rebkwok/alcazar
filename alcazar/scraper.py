b4b0b6b4d211a1d2ff1753e93c004b86ca97564a
Herve Saint-Amand <git@saintamh.org>
2017-11-20 10:18:21 +0000

    Moved cleaner from mixin to composition

--- alcazar/scraper.py
+++ alcazar/scraper.py
@@ -17,26 +17,15 @@ from .fetcher import Fetcher
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
-class Scraper(Cleaner, Fetcher):
+class Scraper(Fetcher):
 
     id = None
     cache_id = None
 
     def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-            else:
-                raise TypeError("Unknown kwarg: %s" % key)
-        if self.id is None:
-            self.id = self.__class__.__name__
-        if self.cache_id is None:
-            self.cache_id = self.id
-        if self.cache_id and self.http_cache_root_path:
-            self.http_cache_root_path = path.join(
-                self.http_cache_root_path,
-                self.cache_id,
-            )
+        self.id = kwargs.pop('id', self.id) or self.__class__.__name__
+        self.cache_id = kwargs('cache_id', self.cache_id) or self.id
+        self.cleaner = Cleaner()
         super(Scraper, self).__init__()
 
     def __call__(self, *args, **kwarg):
