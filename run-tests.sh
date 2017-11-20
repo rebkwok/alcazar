c28c807cbaaf6a2e7971a7c024798e64e4e0e677
Herve Saint-Amand <git@saintamh.org>
2017-11-20 10:15:11 +0000

    run-tests.sh now uses virtualenvs
    But really the real solution would be to use `tox`.

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,13 @@
 
 #------------------------------------------------------------------------------
 
+function python_full_version {
+    cmd="$1"
+    "$cmd" --version 2>&1 | sed 's/Python //'
+}
+
+#------------------------------------------------------------------------------
+
 cd $(dirname $0)
 
 supported_versions=(2.7 3.3 3.4 3.5 3.6)
@@ -16,9 +23,25 @@ exit_status=0
 for v in ${supported_versions[@]}; do
     cmd="python$v"
     if [ $(which $cmd) ]; then
-        full_version=$($cmd --version 2>&1 | sed 's/Python //')
+        full_version=$(python_full_version "$cmd")
         tested_versions+=($full_version)
-        echo "Running tests for Python $full_version"
+        echo "### Python $full_version"
+
+        # setup virtualenv
+        venv_dir="venv-tests-$v"
+        if [ ! -d "$venv_dir" ]; then
+            virtualenv --python=$(which "$cmd") "$venv_dir"
+        fi
+        . "$venv_dir/bin/activate"
+        if [ $(python_full_version 'python') != "$full_version" ]; then
+            echo "virtualenv not properly setup"
+            "$cmd" --version
+            python --version
+            exit 3
+        fi
+        pip install -r requirements.txt || exit "$?"
+
+        # run the tests
         $cmd -m unittest discover -f tests "$@"
         if [ "$?" == "0" ]; then
             summary_entry="pass"
@@ -27,6 +50,9 @@ for v in ${supported_versions[@]}; do
             exit_status=1
         fi
         summary=$(printf "%s\n    %7s: %s" "$summary" "$full_version" "$summary_entry")
+
+        # clean up
+        deactivate
         if [ $exit_status == 1 ]; then
             break
         fi
