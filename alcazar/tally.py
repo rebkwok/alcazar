89f7323178c9aaeccaab9313329530ecbb0dd608
Herve Saint-Amand <git@saintamh.org>
2018-03-18 21:37:42 +0000

    Better tallies

--- alcazar/tally.py
+++ alcazar/tally.py
@@ -4,6 +4,7 @@
 # includes
 
 # standards
+from itertools import chain
 import logging
 
 # alcazar
@@ -20,12 +21,12 @@ class Tally(object):
 
     def __init__(self, log=logging):
         self.log = log
-        self._expected = None
-        self._seen = None
+        self.reset()
 
     def reset(self):
         self._expected = None
-        self._seen = None
+        self._count_by_fate = {}
+        self._count_by_request_type = {}
 
     def set_expected(self, expected):
         if self._expected not in (None, expected):
@@ -34,23 +35,65 @@ class Tally(object):
             self.log.debug('Expected result count set to %d', expected)
         self._expected = expected
 
-    def increment(self, count=1):
-        if self._seen is None:
-            self._seen = 0
-        self._seen += 1
+    def record_request_type(self, request_type, count=1):
+        self._count_by_request_type[request_type] = self._count_by_request_type.get(request_type, 0) + count
+
+    def record_payload_fate(self, fate, count=1):
+        self._count_by_fate[fate] = self._count_by_fate.get(fate, 0) + count
 
     def check(self):
+        self._log_table()
+        total_seen = sum(self._count_by_fate.values())
         if self._expected is not None:
-            if self._seen < self._expected * 0.9:
+            if total_seen < self._expected * 0.9:
                 raise FewerItemsThanExpected("Expected %d results, found %d" % (
                     self._expected,
-                    self._seen,
+                    total_seen,
                 ))
-            elif self._seen != self._expected:
-                self.log.info('Expected %d items, found %d, good enough', self._expected, self._seen)
+            elif total_seen < self._expected:
+                self.log.info('Expected %d items, found %d, good enough', self._expected, total_seen)
             else:
-                self.log.debug('Expected %d items, found %d, hurray', self._expected, self._seen)
+                self.log.debug('Expected %d items, found %d, hurray', self._expected, total_seen)
         else:
-            self.log.info('Expected count not set (seen=%r), tally check not performed', self._seen)
+            self.log.info('Expected count not set (seen=%r), tally check not performed', total_seen)
+
+    def _log_table(self):
+        total = sum(self._count_by_fate.values())
+        table = [
+            line.split('|')
+            for line in chain(
+                self._iter_unpadded_table_lines('Request type', self._count_by_request_type),
+                self._iter_unpadded_table_lines('Payload fate', self._count_by_fate),
+                [
+                    '-- Tally --||',
+                    'expected | %s |' % self._expected if self._expected is not None else '?',
+                    'total | %d | %s' % (
+                        total,
+                        '%.02f%%' % (100.0 * total / self._expected) if self._expected else '',
+                    ),
+                    '-||',
+                ],
+            )
+        ]
+        widths = [
+            max(len(row[i]) for row in table)
+            for i in (0, 1, 2)
+        ]
+        for row in table:
+            pad, sep = '-+' if row[0].startswith('-') else ' |'
+            self.log.info('    ' + sep.join(
+                cell + pad * (width - len(cell))
+                for cell, width in zip(row, widths)
+            ))
+
+    def _iter_unpadded_table_lines(self, title, counts):
+        total = sum(counts.values())
+        yield '-- %s --||' % title
+        for request_type, count in sorted(counts.items()):
+            yield '%s | %-d | %.02f%%' % (
+                request_type,
+                count,
+                100.0 * count / total if total else '-',
+            )
 
 #----------------------------------------------------------------------------------------------------------------------------------
