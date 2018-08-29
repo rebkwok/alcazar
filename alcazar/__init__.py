af64f2d4a2c870cae8f961ea33b71422df0e7c7c
Herve Saint-Amand <git@saintamh.org>
2018-08-29 15:17:09 +0100

    Update version to 0.1.3

--- alcazar/__init__.py
+++ alcazar/__init__.py
@@ -7,7 +7,7 @@
 # 2+3 compat
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 # alcazar
 from .catalogparser import CatalogParser, CatalogResultList
