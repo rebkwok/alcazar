cadcbc14503a623be007aab2daa8c89a063e82c4
Herve Saint-Amand <git@saintamh.org>
2019-01-19 09:51:33 +0000

    Updated test

--- tests/parser_tests/test_xml_parser.py
+++ tests/parser_tests/test_xml_parser.py
@@ -8,7 +8,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # alcazar
-from alcazar.xml_parser import strip_xml_namespaces
+from alcazar.etree_parser import strip_xml_namespaces
 
 # tests
 from .plumbing import AlcazarTest
