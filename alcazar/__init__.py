af490b1b320f572051cd93b4df0a713e37438931
Hervé Saint-Amand <git@saintamh.org>
2019-02-09 06:32:58 +0000

    Add missing imports to global __init__.py

--- alcazar/__init__.py
+++ alcazar/__init__.py
@@ -12,11 +12,15 @@ from .bodytext import ArticleParser, parse_article, parse_body_text
 from .catalogparser import CatalogParser, CatalogResultList
 from .crawler import Crawler
 from .datastructures import GET, Page, POST, Query, Request
+from .etree_parser import parse_html_etree, parse_xml_etree, strip_xml_namespaces
 from .exceptions import AlcazarException, HttpError, HttpRedirect, ScraperError, SkipThisPage
 from .fetcher import Fetcher
 from .forms import Form
 from .http import HttpClient
-from .husker import Husker, HuskerError, HuskerMismatch, HuskerMultipleSpecMatch, HuskerNotUnique, husk
+from .husker import (
+    ElementHusker, Husker, HuskerError, HuskerAttributeNotFound, HuskerLookupError, HuskerMismatch, HuskerMultipleSpecMatch,
+    HuskerNotUnique, JmesPathHusker, ListHusker, NullHusker, ScalarHusker, TextHusker, husk,
+)
 from .scraper import Scraper
 from .skeleton import Skeleton, SkeletonItem
 from .version import ALCAZAR_VERSION
